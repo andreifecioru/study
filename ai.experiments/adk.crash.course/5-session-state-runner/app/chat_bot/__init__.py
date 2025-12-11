5fcd545a150187aed36118a89fe5d540a7f7ea60
Andrei Fecioru <afecioru@adobe.com>
2025-12-11 05:05:57 +0200

    [AI.EXPERIMENTS] - ADK: session & state

---
+++ ai.experiments/adk.crash.course/5-session-state-runner/app/chat_bot/__init__.py
@@ -0,0 +1,5 @@
+from . import agent
+
+__all__ = [
+  "agent",
+]
---
+++ ai.experiments/adk.crash.course/5-session-state-runner/app/chat_bot/agent.py
@@ -0,0 +1,7 @@
+from google.adk.agents import Agent
+
+# create the root_agent
+chat_bot_agent = Agent(
+  name="chat_bot_agent",
+  model="gemini-2.5-flash"
+)
